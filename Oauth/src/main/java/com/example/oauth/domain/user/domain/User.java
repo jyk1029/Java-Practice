40d4367603f99abab15307ca83b4921accacf890
주영 <jyk102999@dsm.hs.kr>
2023-02-19 20:23:17 +0900

    ⚡️ :: Entity 생성

---
+++ Oauth/src/main/java/com/example/oauth/domain/user/domain/User.java
@@ -0,0 +1,41 @@
+package com.example.oauth.domain.user.domain;
+
+import com.example.oauth.domain.user.domain.type.ProviderType;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 64, nullable = false, unique = true)
+    private String email;
+
+    @Column(length = 5, nullable = false)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ProviderType providerType;
+
+    @Builder
+    public User(String email, String name, ProviderType providerType) {
+        this.email = email;
+        this.name = name;
+        this.providerType = providerType;
+    }
+}
