ba1c4461bcdb01d65b768b6c0437bb8457b36e62
주영 <jyk102999@dsm.hs.kr>
2023-02-19 20:35:54 +0900

    ⚡️ :: @EnableConfigurationProperties
    @ConfigurationPropertiesScan에서 변경

--- Oauth/src/main/java/com/example/oauth/OauthApplication.java
+++ Oauth/src/main/java/com/example/oauth/OauthApplication.java
@@ -1,13 +1,14 @@
 package com.example.oauth;
 
+import com.example.oauth.global.security.jwt.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties(value = {JwtProperties.class})
 @SpringBootApplication
 public class OauthApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(OauthApplication.class, args);
     }
-
 }
