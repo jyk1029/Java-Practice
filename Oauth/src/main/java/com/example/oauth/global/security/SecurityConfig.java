f3ec63ccd5a9c333ad29ab6ca4d7e40378c8424e
주영 <jyk102999@dsm.hs.kr>
2023-02-19 18:52:58 +0900

    🔥 :: 안 쓰는 import 삭제

--- Oauth/src/main/java/com/example/oauth/global/security/SecurityConfig.java
+++ Oauth/src/main/java/com/example/oauth/global/security/SecurityConfig.java
@@ -1,10 +1,8 @@
 package com.example.oauth.global.security;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
