9e4dd15d8edc69a137b5ff450d51a20ac95e9f89
주영 <jyk102999@dsm.hs.kr>
2023-02-19 21:12:57 +0900

    ⚡️ :: AuthDetails 추가

---
+++ Oauth/src/main/java/com/example/oauth/domain/user/exception/UserNotFoundException.java
@@ -0,0 +1,13 @@
+package com.example.oauth.domain.user.exception;
+
+import com.example.oauth.global.error.exception.CustomException;
+import com.example.oauth.global.error.exception.ErrorCode;
+
+public class UserNotFoundException extends CustomException {
+    public static final CustomException EXCEPTION =
+            new UserNotFoundException();
+
+    private UserNotFoundException() {
+        super(ErrorCode.USER_NOT_FOUND);
+    }
+}
---
+++ Oauth/src/main/java/com/example/oauth/global/security/auth/AuthDetailsService.java
@@ -0,0 +1,23 @@
+package com.example.oauth.global.security.auth;
+
+import com.example.oauth.domain.user.domain.repository.UserRepository;
+import com.example.oauth.domain.user.exception.UserNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class AuthDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return new AuthDetails(
+                userRepository.findByEmail(email)
+                        .orElseThrow(() -> UserNotFoundException.EXCEPTION)
+        );
+    }
+}
